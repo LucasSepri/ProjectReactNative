a81d1fb80fb9f103ad2a8d74ea94b34330c394a2
LucasSepri <lucassepriano@gmail.com>
2024-10-31 16:58:08 -0300

    9 tentativa

--- backend/src/services/address/ListAddressesService.ts
+++ backend/src/services/address/ListAddressesService.ts
@@ -1,18 +1,19 @@
-import { Endereco } from './../../../node_modules/.prisma/client/index.d';
 // src/services/ListAddressesService.ts
 import prisma from '../../prisma'; // Importe seu cliente Prisma
 
 class ListAddressesService {
   async execute(userId: string) {
-    // Obtém a lista de endereços do usuário
-    const addresses = await prisma.endereco.findMany({
-      where: {
-        userId: userId, // Correção aplicada
-      },
-    });
-
-    return addresses;
+    try {
+      const addresses = await prisma.Endereco.findMany({
+        where: {
+          userId: userId, // Correção aplicada
+        },
+      });
+      return addresses;
+    } catch (error) {
+      console.error('Erro ao buscar endereços:', error);
+      throw new Error('Não foi possível buscar endereços');
+    }
   }
 }
 export default new ListAddressesService();
-
